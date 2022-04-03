ea5d38e63ad94e431dfc097052b0000b351e9b7d
SadiaZubair <69600709+SadiaZubair@users.noreply.github.com>
2022-04-03 14:48:28 +0500

    updated code

--- example.py
+++ example.py
@@ -6,6 +6,8 @@ from urllib.error import URLError
 import random
 import numpy as np
 from copy import deepcopy
+
+
 st.write("--- Data Frame to cluster on ---")
 
 data = pd.read_csv("xclara.csv")
@@ -14,8 +16,7 @@ f1 = list(data['V1'])
 f2 = list(data['V2'])
 X = np.array(list(zip(f1, f2)))
 
-c = alt.Chart(data).mark_circle().encode(
-     x='V1', y='V2')
+c = alt.Chart(data).mark_circle().encode(x='V1', y='V2')
 
 st.altair_chart(c, use_container_width=True)
 cluster_number = st.slider('Number of clusters', 0, 6 , 1)
@@ -26,10 +27,10 @@ def dist(a, b, ax=1):
 # # Number of clusters
 k=cluster_number
 # # X coordinates of random centroids
-C_x = [ random.randint(0,int(max(f1))) for x in range(k)]
+C_x = np.random.randint(0, np.max(f1), size=k)
 
 # # Y coordinates of random centroids
-C_y = [ random.randint(0,int(max(f2))) for x in range(k)]
+C_y = np.random.randint(0, np.max(f2), size=k)
 df = pd.DataFrame({"X":C_x,"Y": C_y})
 
 
@@ -39,41 +40,46 @@ plt.scatter(f1, f2, c='#050505', s=7)
 plt.scatter(C_x, C_y, marker='*', s=200, c='g')
 st.pyplot(fig)
 
-C = np.array(list(zip(f1, f2)))
+C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
 
-print(C)
+# print(C)
 
 # Initizie C_old which will store the value of centroids when it updates
-C_old = np.array(list(zip(C_x, C_y)), dtype=np.float32)
+C_old = np.zeros(C.shape)
 # Intitialize Cluster Lables
 
-clusters = [0 for x in range(len(list(zip(f1, f2))))]
+clusters = []
+for i in range(k):
+    clusters.append([])
 # Error func. - Distance between new centroids and old centroids
-error = dist(C, C_old,None)
-
+error = dist(C_old, C, None)
 # Loop will run till the error becomes zero
 while error != 0:
-
     # Assign each value to its closest cluster
-    for x in range(len(list(zip(f1, f2)))):
-      clusters[x] = np.argmin(dist(X[x],C))
-    # Store the old centroid values
-    C_old = deepcopy(C)   
-    # Find the new centroids by taking the average value
+    clusters = []
     for i in range(k):
-      p = []
-      for j in range(len(X)):
-        if  clusters[j] == i :
-          p.append(X[j])
-      C[i] = np.sum(p, axis=0)/len(p)
-    error = dist(C, C_old, None)
-    
+        clusters.append([]) 
+    for i in range(len(X)):
+        distance = dist(X[i], C)
+        label = np.argmin(distance)
+        clusters[label].append(X[i])
+        
+    # Store the old centroid values
+    C_old = deepcopy(C)
+    # Find the new centroids by taking the average value
+    means = []
+    for i in range(len(clusters)):    
+         means.append(list(np.mean(np.array(clusters[i]), axis=0)))
+    C = np.array(means)
+    error = dist(C_old, C, None)
 
 colors = ['r', 'g', 'b', 'y', 'c', 'm']
 fig1, ax = plt.subplots()
+
+# print(clusters)
 for i in range(k):
-        points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
+        points = np.array(clusters[i])
         ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
-ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='g')
+ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
 
 st.pyplot(fig1)
